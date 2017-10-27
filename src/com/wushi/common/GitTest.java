3f05681820cf447d70d159b2d63ffd78c876af92
hardill <33048787+hardill@users.noreply.github.com>
2017-10-27 13:29:12 +0800

    33

--- src/com/wushi/common/GitTest.java
+++ src/com/wushi/common/GitTest.java
@@ -3,6 +3,6 @@ package com.wushi.common;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("hi11233");
-		System.out.println("hello");
+		System.out.println("hello33");
 	}
 }
